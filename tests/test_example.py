08491ad81fec95ccc57076be285b2fc6a7fa9a57
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-08 00:28:59 +0300

    Add basic opencart tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,34 @@
-def test_example(driver):
-    driver.get("http://192.168.31.140:8081/")
-    assert "Your Store" == driver.title
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_check_logo_on_main(browser):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located((
+        By.CSS_SELECTOR, "#logo"
+    )))
+
+
+def test_check_macbook_page(browser):
+    # url/macbook
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.LINK_TEXT, "Apple")
+    ))
+
+
+def test_check_catalog(browser):
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#content [title='iPhone']")
+    ))
+
+
+def test_check_admin_page(browser):
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "[for='input-username']")
+    ))
+
+
+def test_check_user_registration_page(browser):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "[name = 'firstname']")
+    ))
