ad99932b39a99cdb1e8e82183ed48dd6f747c05f
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-06-08 01:39:48 +0300

    Add new tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -9,14 +9,53 @@ def test_check_logo_on_main(browser):
     )))
 
 
+def test_check_menu_on_main(browser):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located((
+        By.CSS_SELECTOR, ".container #menu"
+    )))
+
+
+def test_check_main_page_title(browser):
+    WebDriverWait(browser, 2).until(EC.title_is("Your Store"))
+
+
+def test_check_main_page_slider(browser):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "div.slideshow")
+    ))
+
+
+def test_check_main_page_product_image(browser):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "[alt='Canon EOS 5D']")
+    ))
+
+
 def test_check_macbook_page(browser):
-    # url/macbook
+    # /macbook
     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
         (By.LINK_TEXT, "Apple")
     ))
 
 
+def test_check_macbook_price(browser):
+    price = WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "div ul h2")
+    ))
+    assert price.text == '$602.00'
+
+
+def test_check_macbook_quantity_input(browser):
+    input1 = WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-quantity")
+    ))
+    assert input1.is_enabled()
+
+
+
+
 def test_check_catalog(browser):
+    # /tablet
     WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
         (By.CSS_SELECTOR, "#content [title='iPhone']")
     ))
